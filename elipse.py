38c52aeaa1959b4673d20cc03f5dd1ee6c69b307
fernandezcanteromanuel-star <fernandezcanteromanuel@gmail.com>
2025-10-30 13:34:49 +0100

    aiaiaiaia

---
+++ elipse.py
@@ -0,0 +1,9 @@
+from figura import Figura
+
+class elipse(Figura):
+    def __init__(self, color="Sin color", eje_may=0, eje_men=0):
+        super().__init__(color)
+        self.eje_may = eje_may
+        self.eje_men = eje_men
+    def __str__(self):
+        return "Se creo una elipse de eje mayor: {}, y eje menor: {}, y de color {}".format(self.eje_may, self.eje_men, self.color)
